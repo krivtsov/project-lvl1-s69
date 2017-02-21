e9ec2d9d279a9782e46ab09a17ae018d86331d9d
kinelit <kinelit@mail.ru>
2017-02-22 01:16:52 +0400

    Step04 fix  - transfer logic

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { welcome, yourName, ruleEvenGame, yourAnswer, sayHello } from '../';
+import { welcome, yourName, ruleEvenGame, iterEvenGame, sayHello } from '../';
 
 
 const gameEven = () => {
@@ -8,28 +8,7 @@ const gameEven = () => {
   ruleEvenGame();
   const name = yourName();
   sayHello(name);
-  const iter = (acc) => {
-    let result = 0;
-    if (acc === 3) {
-      return console.log(`\nCongratulations, ${name}!\n`);
-    }
-    const number = (Math.floor(Math.random() * 10) + 1);
-    console.log(`Question: ${number}`);
-    const checkEven = (num) => {
-      if (num % 2 === 0) {
-        return 'yes';
-      }
-      return 'no';
-    };
-    const answer = yourAnswer();
-    if (answer === checkEven(number)) {
-      result = acc + 1;
-      console.log('Correct!');
-    } else {
-      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${checkEven(number)}'.\nLet's try again, ${name}!`);
-    }
-    return iter(result);
-  };
-  return iter(0);
+  iterEvenGame(name, 0);
 };
+
 gameEven();
--- src/index.js
+++ src/index.js
@@ -18,7 +18,34 @@ export const ruleEvenGame = () => {
   console.log('Answer "yes" if number even otherwise answer "no".\n');
 };
 
+export const ruleCalsGame = () => {
+  console.log('What is the result of the expression?');
+};
+
 export const yourAnswer = () => {
   const answer = readlineSync.question('Your answer: ');
   return answer;
+};
+
+export const iterEvenGame = (name, acc) => {
+  let result = 0;
+  if (acc === 3) {
+    return console.log(`\nCongratulations, ${name}!\n`);
+  }
+  const number = (Math.floor(Math.random() * 27) + 1);
+  console.log(`Question: ${number}`);
+  const checkEven = (num) => {
+    if (num % 2 === 0) {
+      return 'yes';
+    }
+    return 'no';
+  };
+  const answer = yourAnswer();
+  if (answer === checkEven(number)) {
+    result = acc + 1;
+    console.log('Correct!');
+  } else {
+    return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${checkEven(number)}'.\nLet's try again, ${name}!`);
+  }
+  return iterEvenGame(name, result);
 };
