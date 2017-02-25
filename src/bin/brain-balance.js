029158acf14b3758f75dc62d503054e0a73c6194
kinelit <kinelit@mail.ru>
2017-02-26 00:47:22 +0400

    Step07 balance game

---
+++ src/bin/brain-balance.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import runGcdGame from '../games/balance';
+
+runGcdGame();
---
+++ src/games/balance.js
@@ -0,0 +1,29 @@
+import { randomNumber, gameGo } from '..';
+
+const ruleGcdGame = 'Balance the given number.';
+
+const questionGcdGame = () => randomNumber(100, 9999);
+
+const balanceNumber = (arrayNum) => {
+  const maxValue = Math.max.apply(null, arrayNum);
+  const minValue = Math.min.apply(null, arrayNum);
+  const newArrayNum = arrayNum;
+  const maxValueIndex = arrayNum.indexOf(maxValue);
+  const minValueIndex = arrayNum.indexOf(minValue);
+
+  if (maxValue - minValue > 1) {
+    newArrayNum[maxValueIndex] = maxValue - 1;
+    newArrayNum[minValueIndex] = minValue + 1;
+    return balanceNumber(newArrayNum);
+  }
+  return arrayNum.sort();
+};
+
+const correctAnswer = (expression) => {
+  const arrayExp = `${expression}`.split('').map(value => parseInt(value, 10));
+  return Number(balanceNumber(arrayExp).join(''));
+};
+
+export default () => {
+  gameGo(ruleGcdGame, questionGcdGame, correctAnswer);
+};
